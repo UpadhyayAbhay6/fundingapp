d919d1d137ebd99712b7724c94825945dc307500
UpadhyayAbhay7 <abhay9412541844@gmail.com>
2024-07-17 16:39:37 +0530

    fixed useRouter errors

--- app/login/page.js
+++ app/login/page.js
@@ -4,8 +4,9 @@ import { useSession, signIn, signOut } from "next-auth/react"
 import { useRouter } from 'next/navigation'
 const Login = () =>{
     const { data: session } = useSession()
+    const router = useRouter()
     if (session) {
-        const router = useRouter()
+        
         router.push('/dashboard')
     }
   return (
